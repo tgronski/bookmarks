21d82fcd6cc105cfc697bf4f1fd7f6a47c240b7d
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2019-12-04 23:11:55 -0800

    Add validate & fix errors

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || '4bf18994-d42c-4b0c-9e5e-ea1a5ad6e199',
+    
+  }
